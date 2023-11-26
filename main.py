068c26a4e6b65566e3a365af57745ee287aa8903
erwitte <108674109+erwitte@users.noreply.github.com>
2023-11-26 11:26:58 +0100

    calculated correct time from timestamp

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+
+import pytz
+
 from Person import Person
 import folium
 import json
@@ -5,6 +9,14 @@ with open("10.json", "r") as json_file:
     data = json.load(json_file)
 
 from folium.plugins import BeautifyIcon
+
+def millisecondsToCET(mill):
+    utc_time = datetime.utcfromtimestamp(mill)
+    utc_timezone = pytz.timezone("UTC")
+    utc_aware_time = utc_timezone.localize(utc_time)
+    cet_timezone = pytz.timezone("Europe/Berlin")
+    cet_aware_time = utc_aware_time.astimezone(cet_timezone)
+    return cet_aware_time.strftime('%Y-%m-%d %H:%M:%S %Z')
 
 people = []
 routes = []
@@ -18,15 +30,20 @@ for i in range(len(data["daily_routes"][0])):
     for j in range(len(data["daily_routes"][0][i]["coords"])):
         people[i].fillArrays(data["daily_routes"][0][i]["coords"][j], data["daily_routes"][0][i]["times"][j], j)
 
-m = folium.Map(location=(52.27, 8.04))
+m = folium.Map(
+    location=(52.27, 8.04),
+    zoom_start=14
+)
 
 for i in people:
     coordinatesPolyLine =[[]]
 
     for j in range(0, len(i.coords)):
         coordinatesPolyLine[0].append([i.coords[j][0], i.coords[j][1]])
+
         folium.Marker(
-            location=[i.coords[j][0], i.coords[j][1]]
+            location=[i.coords[j][0], i.coords[j][1]],
+            tooltip=millisecondsToCET(i.times[j])
         ).add_to(m)
 
     folium.PolyLine(
