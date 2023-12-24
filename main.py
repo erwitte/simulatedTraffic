84440d73852361ef82961403e53568c4affb9293
erwitte <108674109+erwitte@users.noreply.github.com>
2023-12-24 20:39:07 +0100

    overlay detection during chosen timeperiod done

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 from datetime import datetime
 import pytz
+from jinja2 import Template
+
 from person import Person
 import folium
 import json
@@ -45,13 +47,35 @@ def increase_offset():
 
 def decrease_offset():
     if i.coords[j][0] in existingMarkers[0]:
-        decrease("latitude")
-    existingMarkers[0].append(i.coords[j][0])
+        return decrease("latitude")
 
     if i.coords[j][1] in existingMarkers[1]:
-        decrease("longitude")
-    existingMarkers[1].append(i.coords[j][1])
+        return decrease("longitude")
 
+
+def set_offset():
+    global offset_plus
+    if offset_plus:
+        increase_offset()
+        offset_plus = False
+    else:
+        decrease_offset()
+        offset_plus = True
+
+
+def set_other_offset(coords, times, _id):
+    for i in range(len(existing_coords_times)):
+        if existing_coords_times[i][0] != coords:
+            continue
+        elif _id == existing_coords_times[i][2]:
+            continue
+        else:
+            if existing_coords_times[i][1] == times:
+                continue
+            else:
+                if 0 < existing_coords_times[i][1] - times < 600:
+                    print(str(existing_coords_times[i][0]) + " " + str(coords) + " " + str(existing_coords_times[i][1] - times) + " id: " + str(_id) + " id_ar: "
+                          + str(existing_coords_times[i][2]) + " timestampe_ar: " + str(existing_coords_times[i][1]))
 
 def calculate_color(how_many):
     # 16777148 is FFFFBC is decimal, white is omitted
@@ -108,6 +132,7 @@ def choose_icon(index, person, maximum, _color):
 
 people = []
 routes = []
+existing_coords_times = []
 # 2 arrays for longitude and latitude
 existingMarkers = [[], []]
 # cut off of "0x" from hex
@@ -121,26 +146,47 @@ color_breadth = calculate_color(len(people))
 color = 0
 
 # insert waypoints into upper objects and save indices of their locations (home, work, free time)
-for i in range(len(data["daily_routes"][0])):
-    for j in range(len(data["daily_routes"][0][i]["coords"])):
-        times = data["daily_routes"][0][i]["times"][j]
-        people[i].fill_arrays(data["daily_routes"][0][i]["coords"][j], data["daily_routes"][0][i]["times"][j], j)
+for id in range(len(data["daily_routes"][0])):
+    for coord_or_according_time in range(len(data["daily_routes"][0][id]["coords"])):
+        times = data["daily_routes"][0][id]["times"][coord_or_according_time]
+        people[id].fill_arrays(data["daily_routes"][0][id]["coords"][coord_or_according_time], data["daily_routes"][0][id]["times"][coord_or_according_time], coord_or_according_time)
+        existing_coords_times.append([data["daily_routes"][0][id]["coords"][coord_or_according_time],
+                                      data["daily_routes"][0][id]["times"][coord_or_according_time], data["people"][id]["id"]])
 
 m = folium.Map(
     location=(52.27, 8.04),
     zoom_start=14,
     prefer_canvas=True
 )
-
+#set_other_offset(existing_coords_times[1][0])
 # create and fill map
 
-#todo   hier einfügen!
+test = []
 colors = []
 opacity = 0.8
+offset_plus = True
 
 for i in range(0, len(people)):
     colors.append(color)
     color = color + color_breadth
+    #set_offset()
+
+js = Template(
+    """
+    L.circleMarker({
+    radius: 3,
+    fillOpacity: 1,
+    "lineJoin": "square",
+    })
+    """
+)
+
+js_code = js.render()
+
+for j in range(len(people)):
+    for i in range(len(people[j].coords)):
+        set_other_offset(data["daily_routes"][0][j]["coords"][i], data["daily_routes"][0][j]["times"][i], data["people"][j]["id"])
+
 
 features = [
     {
@@ -151,7 +197,7 @@ features = [
         },
         "properties": {
             "id": str(people[j].id),
-            "tooltip": str(people[j].id) + "<br>" + str(milliseconds_to_cet(people[j].times[i])),
+            "tooltip": str(data["people"][j]["id"]) + "<br>" + str(i+1) + "<br>" + str(milliseconds_to_cet(people[j].times[i])),
             "times": [milliseconds_to_cet(people[j].times[i])[:-4].replace(" ", "T"),
                       milliseconds_to_cet(people[j].times[i+1])[:-4].replace(" ", "T")],
             "style": {
@@ -164,7 +210,10 @@ features = [
                 "fillColor": "#" + str(hex(colors[j])[mySlice]) if people[j].id != 0 else "#000000",
                 "fillOpacity": opacity,
                 "radius": 3,
-            }
+                "line_join": "square",
+                #"iconUrl": "house-solid.svg",
+                #"js": js_code,
+            },
         },
     }
     for j in range(len(people))
