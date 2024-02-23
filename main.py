8cc062b134ac68e954559dfc93b6763a9a87c92b
erwitte <108674109+erwitte@users.noreply.github.com>
2024-02-23 11:40:20 +0000

    detection of overlay dependend on time works

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from jinja2 import Template
 from person import Person
 import folium
 import json
+
 with open("10.json", "r") as json_file:
     data = json.load(json_file)
 from folium.plugins import BeautifyIcon
@@ -65,22 +66,22 @@ def set_offset():
 def set_other_offset(coords, _times, _id):
     is_to_add = True
     for i in range(len(no_overlay)):
-        if no_overlay[i][0] != coords:        # check if coordinates already exist
+        if no_overlay[i][0] != coords:  # check if coordinates already exist
             is_to_add = True
             continue
-        elif _id == no_overlay[i][2]:     # delete to offset same id waypoints
+        elif _id == no_overlay[i][2]:  # delete to offset same id waypoints
             continue
-        else:                           # if coordinates exist in array
-            if no_overlay[i][1] == _times:     # check if times of those coordinates match
+        else:  # if coordinates exist in array
+            if no_overlay[i][1] == _times:  # check if times of those coordinates match
                 is_to_add = False
-                continue                # if so continue to prevent double entry
+                continue  # if so continue to prevent double entry
             else:
                 if 0 < no_overlay[i][1] - _times < 600:
-                    #print(str(test[i][0]) + " " + str(coords) + " " + str(test[i][1] - times) +
-                     #     " id: " + str(_id) + " id_ar: " + str(test[i][2]) + " timestampe_ar: " + str(test[i][1]))
-                    #no_overlay.append([[coords[0]+0.008, coords[1]], _times, _id])
+                    # print(str(test[i][0]) + " " + str(coords) + " " + str(test[i][1] - times) +
+                    #     " id: " + str(_id) + " id_ar: " + str(test[i][2]) + " timestampe_ar: " + str(test[i][1]))
+                    # no_overlay.append([[coords[0]+0.008, coords[1]], _times, _id])
                     is_to_add = False
-                    coords[0] = coords[0] + 0.0009
+                    coords[0] = coords[0] + 0.000015
                     no_overlay.append([coords, _times, _id])
 
     if is_to_add:
@@ -162,16 +163,18 @@ color = 0
 for id in range(len(data["daily_routes"][0])):
     for coord_or_according_time in range(len(data["daily_routes"][0][id]["coords"])):
         times = data["daily_routes"][0][id]["times"][coord_or_according_time]
-        people[id].fill_arrays(data["daily_routes"][0][id]["coords"][coord_or_according_time], data["daily_routes"][0][id]["times"][coord_or_according_time], coord_or_according_time)
+        people[id].fill_arrays(data["daily_routes"][0][id]["coords"][coord_or_according_time],
+                               data["daily_routes"][0][id]["times"][coord_or_according_time], coord_or_according_time)
         existing_coords_times.append([data["daily_routes"][0][id]["coords"][coord_or_according_time],
-                                      data["daily_routes"][0][id]["times"][coord_or_according_time], data["people"][id]["id"]])
+                                      data["daily_routes"][0][id]["times"][coord_or_according_time],
+                                      data["people"][id]["id"]])
 
 m = folium.Map(
     location=(52.27, 8.04),
     zoom_start=14,
     prefer_canvas=True
 )
-#set_other_offset(existing_coords_times[1][0])
+# set_other_offset(existing_coords_times[1][0])
 # create and fill map
 
 no_overlay = []
@@ -182,7 +185,7 @@ offset_plus = True
 for i in range(0, len(people)):
     colors.append(color)
     color = color + color_breadth
-    #set_offset()
+    # set_offset()
 
 js = Template(
     """
@@ -198,39 +201,76 @@ js_code = js.render()
 
 for j in range(len(people)):
     for i in range(len(people[j].coords)):
-        set_other_offset(data["daily_routes"][0][j]["coords"][i], data["daily_routes"][0][j]["times"][i], data["people"][j]["id"])
+        set_other_offset(data["daily_routes"][0][j]["coords"][i], data["daily_routes"][0][j]["times"][i],
+                         data["people"][j]["id"])
 
-
-features = [
+features1 = [
     {
         "type": "Feature",
         "geometry": {
             "type": "LineString",
-            "coordinates": [[people[j].coords[i][1], people[j].coords[i][0]], [people[j].coords[i+1][1], people[j].coords[i+1][0]]],
+            "coordinates": [[people[j].coords[i][1], people[j].coords[i][0]],
+                            [people[j].coords[i + 1][1], people[j].coords[i + 1][0]]],
         },
         "properties": {
             "id": str(people[j].id),
-            "tooltip": str(data["people"][j]["id"]) + "<br>" + str(i+1) + "<br>" + str(seconds_to_cet(people[j].times[i])),
+            "tooltip": str(data["people"][j]["id"]) + "<br>" + str(i + 1) + "<br>" + str(
+                seconds_to_cet(people[j].times[i])),
             "times": [seconds_to_cet(people[j].times[i])[:-4].replace(" ", "T"),
-                      seconds_to_cet(people[j].times[i+1])[:-4].replace(" ", "T")],
+                      seconds_to_cet(people[j].times[i + 1])[:-4].replace(" ", "T")],
             "style": {
                 "color": "#" + str(hex(colors[j])[mySlice]) if people[j].id != 0 else "#000000",
                 "weight": 5,
                 "opacity": opacity,
             },
-            "icon": "circle",  #icon.divIcon
+            "icon": "circle",  # icon.divIcon
             "iconstyle": {
                 "fillColor": "#" + str(hex(colors[j])[mySlice]) if people[j].id != 0 else "#000000",
                 "fillOpacity": opacity,
                 "radius": 3,
                 "line_join": "square",
-                #"iconUrl": "house-solid.svg",
-                #"js": js_code,
+                # "iconUrl": "house-solid.svg",
+                # "js": js_code,
             },
         },
     }
     for j in range(len(people))
     for i in range(len(people[j].coords) - 1)
+]
+# in features sind 3 einträge für coords
+features = [
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "LineString",
+            "coordinates": [
+                [no_overlay[i][0][1], no_overlay[i][0][0]], [no_overlay[i + 1][0][1], no_overlay[i + 1][0][0]]
+            ] if no_overlay[i][2] == no_overlay[i + 1][2] else [[no_overlay[i][0][1], no_overlay[i][0][0]],
+                                                                [no_overlay[i][0][1], no_overlay[i][0][0]]],
+        },
+        "properties": {
+            "id": str(no_overlay[i][2]),
+            "tooltip": str(no_overlay[i][2]) + "<br>" + str(i + 1) + "<br>" + str(seconds_to_cet(no_overlay[i][1])),
+            "times": [seconds_to_cet(no_overlay[i][1])[:-4].replace(" ", "T"),
+                      seconds_to_cet(no_overlay[i + 1][1])[:-4].replace(" ", "T")],
+                        #if no_overlay[i][2] == no_overlay[i + 1][2] else
+                         #   [seconds_to_cet(no_overlay[i][1])[:-4].replace(" ", "T"),
+                          #  seconds_to_cet(no_overlay[i][1])[:-4].replace(" ", "T")],
+            "style": {
+                "color": "#" + str(hex(colors[no_overlay[i][2]])[mySlice]) if no_overlay[i][2] != 0 else "#000000",
+                "weight": 5,
+                "opacity": opacity,
+            },
+            "icon": "circle",
+            "iconstyle": {
+                "fillColor": "#" + str(hex(colors[no_overlay[i][2]])[mySlice]) if people[j].id != 0 else "#000000",
+                "fillOpacity": opacity,
+                "radius": 3,
+                "line_join": "square",
+            },
+        },
+    }
+    for i in range(len(no_overlay) - 1)
 ]
 
 TimestampedGeoJson(
